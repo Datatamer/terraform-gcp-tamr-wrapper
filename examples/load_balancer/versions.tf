22203164f0dac32d8d5a78f3167e9b007b4f370b
creynolds15 <colin.reynolds@tamr.com>
2023-06-20 12:41:24 -0700

    chore: Remove version from provider block

--- examples/load_balancer/versions.tf
+++ examples/load_balancer/versions.tf
@@ -13,11 +13,9 @@ terraform {
 provider "google" {
   project = var.project-id
   region  = "us-east1"
-  version = "~> 3.29.0"
 }
 
 provider "google-beta" {
   project = var.project-id
   region  = "us-east1"
-  version = "~> 3.29.0"
 }
--- examples/minimal/versions.tf
+++ examples/minimal/versions.tf
@@ -1,23 +1,17 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
-    google = {
-      version = "~> 3.29.0"
-    }
-    google-beta = {
-      version = "~> 3.29.0"
-    }
+    google = "~> 3.29.0"
+    google-beta = "~> 3.29.0"
   }
 }
 
 provider "google" {
   project = var.project-id
   region  = "us-east1"
-  version = "~> 3.29.0"
 }
 
 provider "google-beta" {
   project = var.project-id
   region  = "us-east1"
-  version = "~> 3.29.0"
 }
