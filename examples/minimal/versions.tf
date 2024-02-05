4d851e56d3f29049209d819d7533cdf969187b0d
Melissa P <melissapanggwu@gmail.com>
2024-02-05 18:44:39 -0500

    fix: run terraform fmt -recursive

--- examples/minimal/versions.tf
+++ examples/minimal/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     google = {
-      version = "~> 3.29.0" 
+      version = "~> 3.29.0"
     }
     google-beta = {
       version = "~> 3.29.0"
